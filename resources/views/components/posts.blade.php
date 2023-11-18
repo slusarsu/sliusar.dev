bc7c57450ada78b0086e7d6833594160d7f2e1c0
Vitalii Sliusar <slusarvitaliy@gmail.com>
2023-11-18 21:28:28 +0200

    test

--- resources/views/components/posts.blade.php
+++ resources/views/components/posts.blade.php
@@ -4,36 +4,35 @@
 
 @if(!empty($posts))
 
-    <div class="grid grid-cols-1 lg:grid-cols-1 gap-4 ">
-        @foreach($posts as $post)
+    <div class="container-fluid">
 
-            <artile class="bg-white border border-2 rounded p-4">
-                <div class="p-1 text-gray-800">
-                    <a href="{{$post->link()}}">
-                        <h2 class="text-2xl font-semibold text-indigo-950">{{$post->title}}</h2>
-                    </a>
+        <div class="row">
+            <div class="col-sm-12">
+                @foreach($posts as $post)
+                    <div class="card">
+                        <div class="card-header pb-0">
+                            <a href="{{$post->link()}}">
+                                <h5>{{$post->title}}</h5>
+                            </a>
+                        </div>
+                        <div class="card-body">
+                            <p>{!! $post->short !!}</p>
+                        </div>
 
-                    <p class="p-3">{!! $post->short !!}</p>
+                        <div class="d-flex">
+                            <div class="p-2">
+                                {{$post->date()}}
+                            </div>
 
-                </div>
-                <div class="flex justify-between">
-                    <div class="p-2">
-                        {{$post->date()}}
+                            <div class="p-2">
+                                <a href="{{$post->category->link()}}">{{$post->category->title}}</a>
+                            </div>
+                        </div>
                     </div>
+                @endforeach
+            </div>
+        </div>
 
-                    <div class="p-2">
-                        <a href="{{$post->category->link()}}">{{$post->category->title}}</a>
-                    </div>
-                </div>
-
-            </artile>
-
-        @endforeach
-
-    </div>
-
-    <div class="mt-2">
-        {{ $posts->links() }}
     </div>
 
 @endif
--- resources/views/template/post.blade.php
+++ resources/views/template/post.blade.php
@@ -1,15 +1,30 @@
-@extends('template.layouts.app', [
+@extends('template.layouts.main', [
     'title' => $post->seo_title ?? $post->title,
     'seoDescription' => $post->seo_description,
     'seoKeyWords' => $post->seo_text_keys,
-    'sidebar' => true,
     ])
 
 @section('content')
 
-    <article class="bg-white border rounded p-4 text-gray-800">
-        <h2 class="text-2xl font-semibold text-indigo-950 text-center">{{$post->title}}</h2>
-        <p class="pt-4">{!! $post->content !!}</p>
-    </article>
+    <div class="card">
+        <div class="card-header pb-0">
+            <a href="{{$post->link()}}">
+                <h5>{{$post->title}}</h5>
+            </a>
+        </div>
+        <div class="card-body">
+            <p>{!! $post->content !!}</p>
+        </div>
+
+        <div class="d-flex">
+            <div class="p-2">
+                {{$post->date()}}
+            </div>
+
+            <div class="p-2">
+                <a href="{{$post->category->link()}}">{{$post->category->title}}</a>
+            </div>
+        </div>
+    </div>
 
 @endsection
