d291e3bcbd2577386d6df3b021860d15688e0c82
Vitalii Sliusar <slusarvitaliy@gmail.com>
2023-11-25 21:54:39 +0200

    fixed responsive video

--- config/filament-forms-tinyeditor.php
+++ config/filament-forms-tinyeditor.php
@@ -13,14 +13,14 @@ return [
     'profiles' => [
 
         'default' => [
-            'plugins' => 'advlist autoresize codesample directionality emoticons fullscreen hr image imagetools link lists media table toc wordcount',
-            'toolbar' => 'undo redo removeformat | formatselect fontsizeselect | bold italic | rtl ltr | alignjustify alignright aligncenter alignleft | numlist bullist | forecolor backcolor | blockquote table toc hr | image link media codesample emoticons | wordcount fullscreen',
+            'plugins' => 'advlist autoresize codesample directionality emoticons fullscreen hr image imagetools link lists media table toc wordcount code',
+            'toolbar' => 'code | undo redo removeformat | formatselect fontsizeselect | bold italic | rtl ltr | alignjustify alignright aligncenter alignleft | numlist bullist | forecolor backcolor | blockquote table toc hr | image link media codesample emoticons | wordcount fullscreen',
             'upload_directory' => null,
         ],
 
         'simple' => [
-            'plugins' => 'autoresize directionality emoticons link wordcount',
-            'toolbar' => 'removeformat | bold italic | rtl ltr | link emoticons',
+            'plugins' => 'autoresize directionality emoticons link wordcount code',
+            'toolbar' => 'removeformat | bold italic | rtl ltr | link emoticons ',
             'upload_directory' => null,
         ],
 
