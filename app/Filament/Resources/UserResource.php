e838ddc95fc90e9841e07ecb6e4587df5aeeaf0f
Vitalii Sliusar <slusarvitaliy@gmail.com>
2024-03-24 16:44:29 +0200

    added users resource

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,7 +24,7 @@ class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
 
     protected static ?int $navigationSort = 2;
 
@@ -85,7 +85,7 @@ class UserResource extends Resource
 //                            ->multiple()
 //                            ->relationship('roles', 'name')
 //                            ->preload(config('filament-authentication.preload_roles')),
-                    ])
+                    ])->columns(2)
             ]);
     }
 
